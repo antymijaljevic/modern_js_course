6e57335d407017a875858dc868f08672f9b97c45
amijaljevic <antymijaljevic@gmail.com>
2021-04-02 17:18:12 +0100

    new

---
+++ 6_OOP The book List UI/app.js
@@ -0,0 +1,22 @@
+//Book constructor
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+//UI construction
+function UI(){};
+
+
+
+//Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+    const title = document.getElementById('title').value;
+          author = document.getElementById('author').value;
+          isbn = document.getElementById('isbn').value;
+    
+    console.log(title, author, isbn);
+
+    e.preventDefault();
+});

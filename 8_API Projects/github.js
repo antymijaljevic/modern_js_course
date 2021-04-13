6ec858147245a8f6057e2ed55df0bfbc5462cad9
amijaljevic <antymijaljevic@gmail.com>
2021-04-13 19:56:08 +0100

    new

---
+++ 8_API Projects/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'da759bb5dbe19e4544d6';
+    this.client_secret = '5fdf5eae3e5135f93e57607f1008e5aa2bd06f1e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}

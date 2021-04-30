5b3957dd78857eb468a20c3817ccb1c701b011f6
amijaljevic <antymijaljevic@gmail.com>
2021-04-30 15:51:55 +0100

    new

--- 9_Error_handling/app.js
+++ 9_Error_handling/app.js
@@ -1,12 +1,23 @@
 let re;
 
 //shorthand characters classes
+re = /\w/;  //word character - alphanumeric or _
+re = /\w+/; //one or more word characters
+re = /\W/;  //non - word character 
+re = /\d/; // match any digit
+re = /\d+/; // match one or more digits
+re = /\D/; // match any non digit
+re = /\s/; //match whitespace
+re = /\S/; //match non - whitespace
+re = /Hell\b/i; //world boundary: isolate word from sequence
 
-
+//assesrtions
+re = /x(?=y)/; //match x only if followed by y
+re = /x(?!y)/; //match x only if NOT followed by y
 
 
 // String to match
-const str = '3x3x3x3x';
+const str = 'Hello, welcome to Hellxt';
 
 // Log Results
 const result = re.exec(str);
